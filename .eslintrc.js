b763f81c5523bfc021b75e95be8e805c2f6cd254
Alejo Santi <alejoalfredosanti@hotmail.com>
2023-03-05 22:58:09 -0300

    Prettier-lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "linebreak-style": ["error", "windows"], // changes the file to CRLF
+        "vue/multi-word-component-names": 0,
+        "vue/valid-v-slot": "off",
+        "vue/no-unused-vars": "warn",
+        "vue/no-unused-components": "warn",
+    },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,22 @@ import HomeView from "../views/HomeView.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
-  },
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: () => import("../views/AboutView.vue"),
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
-
